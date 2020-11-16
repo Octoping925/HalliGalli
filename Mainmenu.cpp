ab8bf993dd020b1a3f2fdf8cfde8cef2669b637b
kofk113 <64919144+kofk113@users.noreply.github.com>
2020-11-16 23:32:28 +0900

    Add files via upload
    메인 화면 구현

---
+++ Mainmenu.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdlib>
+#include<conio.h>
+using namespace std;
+
+char Menu() {
+
+    char word;
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;    cout << "                                                                                                                                                                                                        " << endl;
+    cout << "   #        #         #           #       #      #        ######           #           #       #      #     " << endl;
+    cout << "   #        #       #   #         #       #             #        #       #   #         #       #            " << endl;
+    cout << "   #        #     #       #       #       #      #      #              #       #       #       #      #     " << endl;
+    cout << "   ##########    ###########      #       #      #      #      ###    ###########      #       #      #     " << endl;
+    cout << "   #        #    #         #      #       #      #      #        #    #         #      #       #      #     " << endl;
+    cout << "   #        #    #         #      #       #      #      #        #    #         #      #       #      #     " << endl;
+    cout << "   #        #    #         #      #       #      #        #######     #         #      #       #      #     " << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+
+
+    cout << "                                           Press the button" << endl;
+    cout << "" << endl;
+
+    cout << "                                              Start:  S" << endl;
+    cout << "                                              Exit :  Q" << endl;
+
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+
+
+    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+     word=_getch() ;// 엔터치지 않아도 바로 입력 받을 수 있게 하기 위해 cin x
+    return word;
+}
+int main() {
+    system("mode con cols=108 lines=40 | title Halli Galli");
+    cout << "" << endl;
+    cout << "" << endl;
+    system("color e9");//e or f is best 
+    cout << "                                          Let's play  Halli Galli "<<endl;
+
+    while (1) {
+        switch (Menu())
+        {
+        case'S': case's':
+
+            break;
+
+        case'Q':case'q':
+            return 0;
+            break;
+        }
+    }
+    return 0;
+}
